8a2dfa381bf2232c5e1b2d153ebcb9407c26ebc5
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-12 17:56:00 -0300

    Fix: model name in singular for coherence.

--- models/jobs.py
+++ models/jobs.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime
 from database import Base
 
 
-class Jobs(Base):
+class Job(Base):
     __tablename__ = "jobs"
 
     id = Column(Integer, primary_key=True, index=True)
