5734e1a0cf827b094210beb9913d691408c20115
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-13 11:21:37 -0300

    API modules

---
+++ api/skills.py
@@ -0,0 +1,41 @@
+from typing import List
+
+import fastapi
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from crud import skills as crud
+from models.database import get_db
+from schemas.skills import Skill
+
+
+router = fastapi.APIRouter()
+
+
+@router.get('/skills', name='all_skills', response_model=List[Skill])
+def get_skills(
+        skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+        ) -> List[Skill]:
+    return crud.get_all(db, skip=skip, limit=limit)
+
+
+@router.post('/skills', name='add_skill', status_code=201, response_model=Skill)
+def create_skill(skill: Skill, db: Session = Depends(get_db)) -> Skill:
+
+    return crud.create(db=db, skill=skill)
+
+
+@router.get('/skills/{skill_id}', name='get_skill')
+def get_skill(skill_id: int, db: Session = Depends(get_db)) -> Skill:
+    db_skill = crud.select_by_id(db, skill_id=skill_id)
+    return db_skill
+
+
+@router.put('/skills/{skill_id}', name='update_skill')
+def update_skill(skill_id: int, db: Session = Depends(get_db)) -> Skill:
+    return Skill
+
+
+@router.delete('/skills/{skill_id}', name='delete_skill')
+def delete_skill(skill_id: int, db: Session = Depends(get_db)) -> Skill:
+    return Skill
