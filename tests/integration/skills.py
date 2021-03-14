e3325909c564b3ffb49224949ddf0985df5216b7
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-14 12:33:23 -0300

    Skills integration tests running.

---
+++ tests/integration/skills.py
@@ -0,0 +1,56 @@
+from tests.utils import client
+import pytest
+
+def test_create():
+    response = client.post(
+        "/skills/",
+        json={
+            "name": "python",
+            "level": 9,
+            "category": "back-end"
+        },
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data["name"] == "python"
+    assert data["level"] == '9'
+    assert data["category"] == "back-end"
+
+
+def test_get_list():
+    response = client.get(f"/skills/")
+    assert response.status_code == 200
+    assert type(response.json()) == list
+
+
+def test_get_by_id():
+    skill_id = 1
+    response = client.get(f"/skills/{skill_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["id"] == skill_id
+
+
+@pytest.mark.skip(reason="Not implemented yet.")
+def test_update():
+    skill_id = 1
+    response = client.put(
+        "/skills/{skill_id}",
+        json={
+            "level": "another@ema.il"
+        },
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["level"] == "another@ema.il"
+
+@pytest.mark.skip(reason="Not implemented yet.")
+def test_delete():
+    skill_id = 1
+    response = client.delete("/skills/{skill_id}")
+
+    assert response.status_code == 200, response.text
+    data = response.json()
+
+    response = client.get(f"/skills/{skill_id}")
+    assert response.status_code == 404, None
