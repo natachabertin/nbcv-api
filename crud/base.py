30dffb69cbc0d83618285d5d0533342b02892932
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-14 14:37:53 -0300

    CRUD base: list_items unified

--- crud/base.py
+++ crud/base.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 from models.database import Base as Model
@@ -5,3 +7,9 @@ from models.database import Base as Model
 
 def select_item_by_id(db: Session, entity: Model, entity_id: int) -> Model:
     return db.query(entity).filter(entity.id == entity_id).first()
+
+
+def list_items(
+        db: Session, entity: Model, skip: int = 0, limit: int = 100
+        ) -> List[Model]:
+    return db.query(entity).offset(skip).limit(limit).all()
