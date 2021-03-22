2d780d5adb83c41454bb582d951c883001ac5646
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-21 21:32:10 -0300

    tests/utils.py: added for util functs on tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,29 +1,3 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from models.database import Base, get_db
-from main import api
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine
-)
-
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-api.dependency_overrides[get_db] = override_get_db
-
-client = TestClient(api)
+def get_the_first_id(entity, client):
+    response = client.get(f"/{entity}/")
+    return response.json()[0]['id']
