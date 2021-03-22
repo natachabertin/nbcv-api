1243c4b47dea0e43017a6457141c99de9923b38c
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-21 22:22:19 -0300

    Jobs: Delete implemented E2E.

--- crud/jobs.py
+++ crud/jobs.py
@@ -2,7 +2,8 @@ from typing import List
 
 from sqlalchemy.orm import Session
 
-from crud.base import select_item_by_id, list_items, create_item, update_item
+from crud.base import select_item_by_id, list_items, create_item, update_item, \
+    delete_item
 from models.jobs import Job as mJob
 from schemas.jobs import Job as sJob
 
@@ -25,5 +26,5 @@ def update(
     return update_item(db, job_submit, mJob, job_id)
 
 
-def delete(db: Session, job_id: int, job: mJob) -> mJob:
-    return update_item(db, sJob, job, job_id)
+def delete(db: Session, job_id: int):
+    return delete_item(db, mJob, job_id)
