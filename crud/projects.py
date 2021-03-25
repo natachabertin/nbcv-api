c8f768d2dd252b003b584cc5c205b09ba4f87ffd
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-25 15:25:56 -0300

    Projects: validated model E2E.

--- crud/projects.py
+++ crud/projects.py
@@ -2,26 +2,27 @@ from typing import List
 
 from sqlalchemy.orm import Session
 
-from crud.base import select_item_by_id, list_items, create_item, update_item, \
-    delete_item
+from crud.base import (
+    select_item_by_id, list_items, create_item, update_item, delete_item
+)
 from models.projects import Project as mProject
-from schemas.projects import Project as sProject
+from schemas.projects import Project, ProjectCreate, ProjectUpdate
 
 
 def select_by_id(db: Session, project_id: int) -> mProject:
     return select_item_by_id(db, mProject, project_id)
 
 
-def get_all(db: Session, skip: int = 0, limit: int = 100) -> List[mProject]:
+def get_all(db: Session, skip: int = 0, limit: int = 100) -> List[Project]:
     return list_items(db, mProject, skip, limit)
 
 
-def create(db: Session, project: sProject) -> mProject:
+def create(db: Session, project: ProjectCreate) -> mProject:
     return create_item(db, project, mProject)
 
 
 def update(
-        db: Session, project_id: int, project_submit: sProject
+        db: Session, project_id: int, project_submit: ProjectUpdate
         ) -> mProject:
     return update_item(db, project_submit, mProject, project_id)
 
--- schemas/projects.py
+++ schemas/projects.py
@@ -1,14 +1,29 @@
-from datetime import datetime
+from datetime import date
 from typing import Optional
 
 from pydantic import BaseModel
 
 
-class Project(BaseModel):
+class ProjectBase(BaseModel):
     name: str
     description: str
-    start_date: Optional[datetime]
-    end_date: Optional[datetime]
+    start_date: date
+    end_date: date
+
+
+class ProjectCreate(ProjectBase):
+    pass
+
+
+class ProjectUpdate(ProjectBase):
+    name: Optional[str]
+    description: Optional[str]
+    start_date: Optional[date]
+    end_date: Optional[date]
+
+
+class Project(ProjectBase):
+    id: int
 
     class Config:
         orm_mode = True
