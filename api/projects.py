21c21b47a2ef88231a4fc78a385deb0a56d641cf
NatachaBertinCowan <natachabertin92@gmail.com>
2021-03-29 18:25:09 -0300

    Projs API: added superuser restrictions for non read operations.

--- api/projects.py
+++ api/projects.py
@@ -2,6 +2,7 @@ from typing import List
 
 import fastapi
 from fastapi import Depends, Response
+from security.jwt_auth import is_active_superuser
 from sqlalchemy.orm import Session
 
 from crud import projects as crud
@@ -19,7 +20,8 @@ def get_projects(
     return crud.get_all(db, skip=skip, limit=limit)
 
 
-@router.post('/', name='add_project', status_code=201, response_model=Project)
+@router.post('/', name='add_project', status_code=201, response_model=Project,
+             dependencies=[Depends(is_active_superuser())])
 def create_project(project: ProjectCreate, db: Session = Depends(get_db)) -> Project:
 
     return crud.create(db=db, project=project)
@@ -31,11 +33,13 @@ def get_project(project_id: int, db: Session = Depends(get_db)) -> Project:
     return db_project
 
 
-@router.patch('/{project_id}', name='update_project', response_model=Project)
+@router.patch('/{project_id}', name='update_project', response_model=Project,
+             dependencies=[Depends(is_active_superuser())])
 def update_project(project_id: int, project: ProjectUpdate, db: Session = Depends(get_db)) -> Project:
     return crud.update(db=db, project_id=project_id, project_submit=project)
 
 
-@router.delete('/{project_id}', name='delete_project', status_code=204, response_class=Response)
+@router.delete('/{project_id}', name='delete_project', status_code=204, response_class=Response,
+             dependencies=[Depends(is_active_superuser())])
 def delete_project(project_id: int, db: Session = Depends(get_db)):
     return crud.delete(db=db, project_id=project_id)
